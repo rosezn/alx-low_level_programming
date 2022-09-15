b85832a545020bbe6528eafc569906fac7977809
rose zwelibanzi <rosezwelibanzi@gmail.com>
2022-09-14 21:46:06 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-in_putchar(char);
+int_putchar(char);
 void print_alphabet(void);
-void print_alphabet_x10(void);
+void print_aphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
