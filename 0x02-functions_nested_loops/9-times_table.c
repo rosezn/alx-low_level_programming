e1bcd39fd014395830782077e0d39545fe3cba78
rose zwelibanzi <rosezwelibanzi@gmail.com>
2022-09-15 22:36:47 -0700

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 0; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
@@ -18,10 +18,11 @@ void times_table(void)
 
 			prod = num * mult;
 
-			if (prod <= 9) 
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
