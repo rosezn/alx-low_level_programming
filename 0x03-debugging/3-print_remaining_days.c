fb0b9b98cbfbcb621e574fad6c47e1f42917daff
rose zwelibanzi <rosezwelibanzi@gmail.com>
2022-09-20 12:37:53 -0700

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 
 /**
  * print_remaining_days - takes a date and prints how many days
- * are lef in the year, tkaing leap years into account
+ * are left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
  * @year: year
