fc21003a7fbba49dac8e7be3e4c0fc3e121fe246
rose zwelibanzi <rosezwelibanzi@gmail.com>
2022-09-20 15:35:30 -0700

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,7 @@
 void print_triangle(int size)
 {
 	int inc1, inc2;
-	
+
 	if (size > 0)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
@@ -24,6 +24,7 @@ void print_triangle(int size)
 			{
 				putchar('#');
 			}
+
 			if (inc1 == size)
 			{
 				continue;
