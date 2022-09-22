4a0b574af500cc86d055552d37e2c0b603690574
rose zwelibanzi <rosezwelibanzi@gmail.com>
2022-09-21 20:09:40 -0700

    String length

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,14 +4,13 @@
  * swap_int - swaps two integers' values
  * @a: first integer
  * @b: second integer
- *
  * Return: returns nothing
  */
 
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
-	
+
 	*a = *b;
 	*b = tmp;
 }
