b8ceefeb2b031aab7d4a38665d1684df2db08d36
Rose Zwelibanzi <rosezwelibanzi@gmail.com>
2022-12-29 09:47:26 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,8 @@
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * this program prints "Programming is positive, zero, or negative"
+ *
  * Return: 0
  */
 
